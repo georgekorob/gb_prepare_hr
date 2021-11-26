e122692dfce07b732b576d093b60012926532ae7
George <georgekorob@gmail.com>
2021-11-26 12:47:37 +0300

    add site field to ProductCategory

--- djproject/djapp/models/models.py
+++ djproject/djapp/models/models.py
@@ -5,8 +5,7 @@ from django.db import models
 # Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64)
-
-    # Модифицировать модель раздела так, чтобы ее можно было подключить к какому то одному сайту.
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
@@ -20,8 +19,6 @@ class Product(models.Model):
     provider_name = models.CharField(max_length=64, blank=True)
     categories = models.ManyToManyField(ProductCategory, related_name='categories')
     site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
-
-    # Модифицировать модель товара так, чтобы ее можно было привязать к одному или нескольким сайтам.
 
     def __str__(self):
         return self.name
